dd376010c01d5397562d3c2edb579472285c810f
RaffaRaihan <kaizyexercise@gmail.com>
2024-08-02 15:00:50 +0700

    "Pembetulan di bagian Company Logo"

--- components/Company.vue
+++ components/Company.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="company-logo mx-[100px] mt-[70px] w-[1240px] flex justify-between">
-        <NuxtLink href="#" class="w-[124,11px] h-[48px] pr-[100px]"><img src="../assets/vectors/Amazon.svg" alt="Amazon"></NuxtLink>
-        <NuxtLink href="#" class="w-[124,11px] h-[48px] pr-[100px]"><img src="../assets/vectors/Driblle.svg" alt="Driblle"></NuxtLink>
-        <NuxtLink href="#" class="w-[124,11px] h-[48px] pr-[100px]"><img src="../assets/vectors/HubSpot.svg" alt="HubSpot"></NuxtLink>
-        <NuxtLink href="#" class="w-[124,11px] h-[48px] pr-[100px]"><img src="../assets/vectors/Notion.svg" alt="Notion"></NuxtLink>
-        <NuxtLink href="#" class="w-[124,11px] h-[48px] pr-[100px]"><img src="../assets/vectors/Netflix.svg" alt="Netflix"></NuxtLink>
-        <NuxtLink href="#" class="w-[124,11px] h-[48px] pr-[100px]"><img src="../assets/vectors/Zoom.svg" alt="Zoom"></NuxtLink>
+    <div class="company-logo mx-[100px] mt-[70px] w-[1240px] flex justify-between gap-[10px]">
+        <NuxtLink href="#" class="w-[124.11px] h-[48px]"><img src="../assets/vectors/Amazon.svg" alt="Amazon"></NuxtLink>
+        <NuxtLink href="#" class="w-[124.11px] h-[48px]"><img src="../assets/vectors/Driblle.svg" alt="Driblle"></NuxtLink>
+        <NuxtLink href="#" class="w-[124.11px] h-[48px]"><img src="../assets/vectors/HubSpot.svg" alt="HubSpot"></NuxtLink>
+        <NuxtLink href="#" class="w-[124.11px] h-[48px]"><img src="../assets/vectors/Notion.svg" alt="Notion"></NuxtLink>
+        <NuxtLink href="#" class="w-[124.11px] h-[48px]"><img src="../assets/vectors/Netflix.svg" alt="Netflix"></NuxtLink>
+        <NuxtLink href="#" class="w-[124.11px] h-[48px]"><img src="../assets/vectors/Zoom.svg" alt="Zoom"></NuxtLink>
     </div>
 </template>
